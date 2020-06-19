c5fcd1432bb2d1d416ef93e27465b174f203f7a8
Akshay Krishna <akshaykri@gmail.com>
2020-06-19 12:31:40 -0400

    Pair correlation function g(r) works, examples added

--- QH_ED/hilbert.py
+++ QH_ED/hilbert.py
@@ -349,6 +349,132 @@ def dijToCsr(dij, NH):
         
     return H
 
+@numba.njit(numba.complex128[:](numba.complex128[:],
+                             numba.int64,
+                             numba.int64,
+                             numba.int64,
+                             numba.int8[:,:],
+                             numba.int64[:],
+                             numba.complex128[:,:],
+                             numba.types.DictType(numba.int64, numba.int64)),
+            parallel=True, fastmath=True)
+def getMatVecC(v, Nphi, Ne, NH, hilb, hilbLen, T4, dictx):
+    """
+    Hamiltonian operator (shell matrix)
+    about 300x slower than getMat, but saves tremendous memory
+        
+    inputs:
+    -------
+    v        : np.array (float of size NH)
+    Nphi     : int, number of orbitals
+    Ne       : int, number of electrons
+    NH       : int, size of Hilbert space
+    hilb     : np.array of ints, of NH rows and Ne columns with the 
+               full Hilbert space
+    hilbLen  : np.array of ints, size of Hilbert space in each momentum subsector
+    T4       : np.array of size Nphi x Nphi with elements of the interaction tensor
+    dictx    : hashmap: keys are decimal representation of electron occupancies,
+                        e.g. Ne = [0, 1, 3, 6] = 1 + 2 + 8 + 64 = 75
+                        values are array indices of the state in hilb
+        
+    outputs:
+    --------
+    vOut: np.array (float of size NH), result of Hv
+    """
+        
+    vOut = np.zeros(NH, dtype=numba.complex128)
+
+    for cHilb in numba.prange(NH):
+        eOcc = hilb[cHilb] # occupied electrons
+            
+        diagel = 0.0
+            
+        #find pairs
+        for p1 in np.arange(Ne):
+            for p2 in np.arange(p1+1, Ne):
+                c1 = eOcc[p1] # orbital of first occupied electron
+                c2 = eOcc[p2] # orbital of second occupied electron
+    
+                # diagonal terms
+                diagel += -T4[(c1-c2)%Nphi, (c1-c2)%Nphi] + \
+                          -T4[(c2-c1)%Nphi, (c2-c1)%Nphi] + \
+                           T4[(c1-c2)%Nphi, 0] + \
+                           T4[(c2-c1)%Nphi, 0]
+                                                   
+                # find new pairs
+                for cx in np.arange(1, Nphi):
+                    c1new = (c1+cx)%Nphi
+                    c2new = (c2-cx)%Nphi
+                    if c1new >= c2new:
+                        continue
+                            
+#                     if c1new in eOcc or c2new in eOcc:
+#                         continue
+
+                    ####################
+#                     numba does not recognize contains (the 'in' keyword)
+                    searchIn = False
+                    for eOrb in eOcc:
+                        if c1new == eOrb or c2new == eOrb:
+                            searchIn = True
+                            break
+                        if searchIn:
+                            continue
+                    ####################
+                        
+                    else:
+                        # at this point, we are sure that c2new > c1new,
+                        # and that they are both not in eOcc
+
+                        # populate eOccNew
+#                         eOccNew = np.zeros(self.Ne, dtype='int8') # array of length Ne
+                        eOccNew = np.zeros(Ne, dtype=numba.int64) # array of length Ne
+                        cNeOld = 0
+                        state = 0
+                        for cNe in range(Ne):
+                            while cNeOld == p1 or cNeOld == p2:
+                                cNeOld += 1
+
+                            if state == 0:
+                                if cNeOld < Ne and eOcc[cNeOld] < c1new:
+                                    eOccNew[cNe] = eOcc[cNeOld]
+                                    cNeOld += 1
+                                else:
+                                    eOccNew[cNe] = c1new
+                                    p1new = cNe
+                                    state = 1
+
+                            elif state == 1:
+                                if cNeOld < Ne and eOcc[cNeOld] < c2new:
+                                    eOccNew[cNe] = eOcc[cNeOld]
+                                    cNeOld += 1
+                                else:
+                                    eOccNew[cNe] = c2new
+                                    p2new = cNe
+                                    state = 2
+
+                            else:
+                                eOccNew[cNe] = eOcc[cNeOld]
+                                cNeOld += 1
+
+
+                        # find index of eOccNew in hilb, again binary search
+                        indNew = dictx[np.sum(2**eOccNew)] # self.indexOf(eOccNew)
+                            
+                        # (-1)^n = 1 - 2*(n%2)
+                        #the four terms
+                        matrixel = ((1 - 2*((p1new+p2new+p1+p2 + 1)%2)) * 
+                                    (T4[(c1new-c2new)%Nphi, (c1-c2new)%Nphi] + \
+                                     T4[(c2new-c1new)%Nphi, (c2-c1new)%Nphi])) + \
+                                   ((1 - 2*((p1new+p2new+p1+p2)%2)) * 
+                                    (T4[(c1new-c2new)%Nphi, (c2-c2new)%Nphi] + \
+                                     T4[(c2new-c1new)%Nphi, (c1-c1new)%Nphi]))
+                            
+                        # update vOut
+                        vOut[cHilb] += matrixel * v[indNew]
+        vOut[cHilb] += diagel * v[cHilb]
+    return vOut
+
 @numba.njit(numba.types.Tuple((numba.int64[:,:], numba.complex128[:]))
                 (numba.int64,
                  numba.int64,
